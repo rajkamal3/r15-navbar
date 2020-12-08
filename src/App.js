2eead18827d13a2d441871803681e45fcc050cd2
rajkamal3 <rkchenumalla@gmail.com>
2020-12-08 16:28:15 +0530

    initial/final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// 5:17:20
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default App;
